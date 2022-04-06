a15e7e1bf784d2896c90dbbcb11fc466f8900a3a
Adnan Mahmić <mahmica@gmail.com>
2022-04-06 18:35:52 +0200

    accounts project

---
+++ Accounts-project/db.js
@@ -0,0 +1,16 @@
+let db = [
+    {
+        id : 1,
+        name: "Adnan",
+        lastname: "Mahmic",
+        email: "mahmica@gmail.com",
+        phone: "11-11-11-11"
+    },
+    {
+        id : 2,
+        name: "Danilo",
+        lastname: "Vesovic",
+        email: "danilo@gmail.com",
+        phone: "22-33-22-22"
+    },
+];
