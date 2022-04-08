be4d4c9b0986e3f5343bf9681450ec275fa0691e
Adnan Mahmić <mahmica@gmail.com>
2022-04-08 18:07:53 +0200

    implement add account mechanism

--- Accounts-project/main.js
+++ Accounts-project/main.js
@@ -1,27 +1,52 @@
 let accountsTableBody = document.querySelector("#accounts-table-body");
 let allLinks = document.querySelectorAll('.nav-link');
-
 let accountsView = document.querySelector('#accounts-view');
 let addAccountView = document.querySelectorAll('#add-account-view');
 let views = document.querySelectorAll('.view');
+let idInput = document.querySelector('[placeholder="id"]');
+let nameInput = document.querySelector('[placeholder="name"]');
+let lastNameInput = document.querySelector('[placeholder="lastname"]');
+let emailInput = document.querySelector('[placeholder="email"]');
+let phoneInput = document.querySelector('[placeholder="phone"]');
+let saveBtn = document.querySelector('#save');
+
+saveBtn.addEventListener('click', saveAccount);
+
+function saveAccount() {
+    const newAccount = {
+        id: idInput.value,
+        name: nameInput.value,
+        lastName: lastNameInput.value,
+        email: emailInput.value,
+        phone: phoneInput.value,
+    }
+    db.push(newAccount);
+    idInput.value = "";
+    nameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    phoneInput.value = "";
+    createAccountsTable();
+    showView("#accounts-view");
+}
 
 for (let i = 0; i < allLinks.length; i++) {
     allLinks[i].addEventListener('click', showView);
 }
 
 function showView(e) {
-    e.preventDefault();
     for (let i = 0; i < views.length; i++) {
         views[i].style.display = "none";
-
     }
-    let id = `#${this.getAttribute("href")}`;
 
-    document.querySelector(id).style.display = "block";
+    if (e instanceof Event) {
+        e.preventDefault();
+        let id = `#${this.getAttribute("href")}`;
+        document.querySelector(id).style.display = "block";
+    } else {
+        document.querySelector(e).style.display = "block";
+    }
 }
-
-
-
 
 createAccountsTable();
 
