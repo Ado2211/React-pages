ebf4af6958c4057fe0d4baccfb06974facef0543
Adnan Mahmić <mahmica@gmail.com>
2022-04-09 17:47:45 +0200

    implement delete, edit and save mechanisms

--- Accounts-project/db.js
+++ Accounts-project/db.js
@@ -1,16 +1,5 @@
-let db = [
-    {
-        id : 1,
-        name: "Adnan",
-        lastname: "Mahmic",
-        email: "mahmica@gmail.com",
-        phone: "11-11-11-11"
-    },
-    {
-        id : 2,
-        name: "Danilo",
-        lastname: "Vesovic",
-        email: "danilo@gmail.com",
-        phone: "22-33-22-22"
-    },
-];
+let db = [];
+
+if(localStorage.db){
+    db= JSON.parse(localStorage.db);
+}
--- Accounts-project/main.js
+++ Accounts-project/main.js
@@ -1,3 +1,5 @@
+window.addEventListener('beforeunload',save);
+
 let accountsTableBody = document.querySelector("#accounts-table-body");
 let allLinks = document.querySelectorAll('.nav-link');
 let accountsView = document.querySelector('#accounts-view');
@@ -9,14 +11,35 @@ let lastNameInput = document.querySelector('[placeholder="lastname"]');
 let emailInput = document.querySelector('[placeholder="email"]');
 let phoneInput = document.querySelector('[placeholder="phone"]');
 let saveBtn = document.querySelector('#save');
+let eId = document.querySelector('.eId');
+let eName = document.querySelector('.eName');
+let eLastName = document.querySelector('.eLastName');
+let eEmail = document.querySelector('.eEmail');
+let ePhone = document.querySelector('.ePhone');
+let editBtn = document.querySelector('#edit');
+let id; 
 
+editBtn.addEventListener('click', saveEditedAccount);
 saveBtn.addEventListener('click', saveAccount);
+
+function saveEditedAccount() {
+    const editedAccount = {
+        id: eId.value,
+        name: eName.value,
+        lastname: eLastName.value,
+        email: eEmail.value,
+        phone: ePhone.value
+    }
+    db[id] = editedAccount;
+    createAccountsTable();
+    showView("#accounts-view");
+}
 
 function saveAccount() {
     const newAccount = {
         id: idInput.value,
         name: nameInput.value,
-        lastName: lastNameInput.value,
+        lastname: lastNameInput.value,
         email: emailInput.value,
         phone: phoneInput.value,
     }
@@ -61,8 +84,39 @@ function createAccountsTable() {
         <td>${account.lastname}</td>
         <td>${account.email}</td>
         <td>${account.phone}</td>
+        <td><button data-id="${i}" class="edit-btn btn btn-sm btn-warning form-control">Edit</button></td>
+        <td><button data-id="${i}" class="delete-btn btn btn-sm btn-danger form-control">Delete</button></td>
     </tr>
         `
     }
-    accountsTableBody.innerHTML = htmlAccounts
+    accountsTableBody.innerHTML = htmlAccounts;
+    let allDeleteBtns = document.querySelectorAll('.delete-btn');
+    let allEditBtns = document.querySelectorAll('.edit-btn');
+
+    for (let i = 0; i < allDeleteBtns.length; i++) {
+        allDeleteBtns[i].addEventListener('click', deleteAccount);
+        allEditBtns[i].addEventListener('click', editAccount);
+    }
+}
+
+function deleteAccount() {
+    let id = this.getAttribute('data-id');
+    db.splice(id, 1);
+    createAccountsTable();
+    showView("#accounts-view");
+}
+
+function editAccount() {
+    id = this.getAttribute('data-id');
+    let selectedAccount = db[id];
+    eId.value = selectedAccount.id;
+    eName.value = selectedAccount.name;
+    eLastName.value = selectedAccount.lastname;
+    eEmail.value = selectedAccount.email;
+    ePhone.value = selectedAccount.phone;
+    showView("#edit-account-view");
+}
+
+function save() {
+    localStorage.db = JSON.stringify(db);
 }
